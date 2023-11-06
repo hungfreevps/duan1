ba20e9df0d05ac4fe08590e31ae5bbebc9665814
dongtx <dongt20044@gmail.com>
2023-11-06 19:05:44 +0700

    x

--- index.php
+++ index.php
@@ -5,9 +5,13 @@ include "model/pdo.php";
 
 if((isset($_GET['act'])) && ($_GET['act']!="")){
     $act = $_GET['act'];
-    switch ($act){}
+    switch ($act){
+        case 'login':
+            include "view/login.php";
+            break;
+    }
 } else {
-    include "../view/home.php";
+    include "view/home.php";
 }
-include "../view/footer.php";
+include "view/footer.php";
 ?>
