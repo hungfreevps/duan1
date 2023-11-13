a4e842daede304c8ec1d542bdc6f4eebe835c356
lamhung <hungvlph35661@fpt.edu.vn>
2023-11-13 19:23:18 +0700

    ok

--- view/header.php
+++ view/header.php
@@ -1,25 +1,39 @@
 <!doctype html>
 <html>
 
+<!-- Mirrored from amovie.gozha.net/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 01 Nov 2023 12:56:49 GMT -->
+<head>
+	<!-- Basic Page Needs -->
         <meta charset="utf-8">
-        <title>Movie</title>
+        <title>AMovie</title>
         <meta name="description" content="A Template by Gozha.net">
         <meta name="keywords" content="HTML, CSS, JavaScript">
-        <meta name="author" content="Gozha.net"> 
+        <meta name="author" content="Gozha.net">
+    
+    <!-- Mobile Specific Metas-->
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta content="telephone=no" name="format-detection">
+    
     <!-- Fonts -->
         <!-- Font awesome - icon font -->
-        <link href="../netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+        <link href="netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
         <!-- Roboto -->
         <link href='http://fonts.googleapis.com/css?family=Roboto:400,100,700' rel='stylesheet' type='text/css'>
         <!-- Open Sans -->
         <link href='http://fonts.googleapis.com/css?family=Open+Sans:800italic' rel='stylesheet' type='text/css'>
     
     <!-- Stylesheets -->
+        <!-- jQuery UI --> 
+        <link href="../code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css" rel="stylesheet">
 
         <!-- Mobile menu -->
         <link href="css/gozha-nav.css" rel="stylesheet" />
         <!-- Select -->
         <link href="css/external/jquery.selectbox.css" rel="stylesheet" />
+        <!-- Swiper slider -->
+        <link href="css/external/idangerous.swiper.css" rel="stylesheet" />
+        <!-- Magnific-popup -->
+        <link href="css/external/magnific-popup.css" rel="stylesheet" />
 
         <!-- REVOLUTION BANNER CSS SETTINGS -->
         <link rel="stylesheet" type="text/css" href="rs-plugin/css/settings.css" media="screen" />
@@ -30,6 +44,8 @@
 
         <!-- Modernizr --> 
         <script src="js/external/modernizr.custom.js"></script>
+        <script src="https://kit.fontawesome.com/509cc166d7.js" crossorigin="anonymous"></script>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
     
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries --> 
     <!--[if lt IE 9]> 
@@ -37,6 +53,7 @@
 		<script src="http://cdnjs.cloudflare.com/ajax/libs/respond.js/1.3.0/respond.js"></script>		
     <![endif]-->
 </head>
+
 <body>
     <div class="wrapper">
         <!-- Banner -->
@@ -48,7 +65,7 @@
         <header class="header-wrapper header-wrapper--home">
             <div class="container">
                 <!-- Logo link-->
-                <a href='index.html' class="logo">
+                <a href='index.php' class="logo">
                     <img alt='logo' src="images/logo.png">
                 </a>
                 
@@ -67,7 +84,7 @@
                     <ul id="navigation">
                         <li>
                             <span class="sub-nav-toggle plus"></span>
-                            <a href="#">Trang Chủ</a>
+                            <a href="index.php">Trang Chủ</a>
                         </li>
 
                         <li>
@@ -89,6 +106,7 @@
                             <span class="sub-nav-toggle plus"></span>
                             <a href="#">Mới</a>
                         </li>
+                        
 
                         <!-- <li>
                             <span class="sub-nav-toggle plus"></span>
@@ -99,27 +117,14 @@
                 
                 <!-- Additional header buttons / Auth and direct link to booking-->
                 <div class="control-panel">
-                    <!-- <div class="auth auth--home">
-                      <div class="auth__show">
-                        <span class="auth__image">
-                          <img alt="" src="../images/client-photo/auth.png">
-                        </span>
-                      </div>
-                      <a href="#" class="btn btn--sign btn--singin">
-                          me
-                      </a>
-                        <ul class="auth__function">
-                            <li><a href="#" class="auth__function-item login-window">đăng nhập</a></li>
-                            <li><a href="../login.html" class="auth__function-item">đăng ký</a></li>
-                        </ul>
-
-                    </div> -->
-                    <div class="btn btn-md btn--warning btn--book btn-control--home login-window">
-                        <span>
-                       <a href="login.html" class="auth__function-item login-window">Đăng Nhập</a>
-                        </span>
+                    <div class="btn btn-md btn--warning btn--book btn-control--home">
+                      <span>
+                      <a href="index.php?act=dangnhap" class="">Đăng Nhập</a>
+                      </span>
+                        
+                      
                     </div>
-                    <a href="#" class="btn btn-md btn--warning btn--book btn-control--home login-window">Đặt Vé</a>
+                    <a href="#" class="btn btn-md btn--warning btn--book btn-control--home">Đặt Vé</a>
                 </div>
 
             </div>
